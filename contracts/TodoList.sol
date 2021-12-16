c0c5af8ad455b5de490350e7037eae2495cdf113
ropats16 <rohitcpathare@gmail.com>
2021-12-16 18:46:10 +0530

    1. Project Setup

---
+++ contracts/TodoList.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.5.0;
+//Declaring the Solidity version
+
+contract TodoList {
+	//Declaring the smart contract with name "TodoList"
+	uint public taskCount = 0;
+	//uint initiates a state variable
+	//taskCount will keep track of the number of task
+	//Default Value 0 assigned to taskCount
+	//"public" offers a way to read this value from 
+	//the smart contract
+	//"public" keyword creates a function taskCount() that
+	//lets us access its value outside the contract
+}
+
